2c475b48b9b91f650824f1ab2c65c2900ced48a3
Gabriel1373 <90777523+Gabriel1373@users.noreply.github.com>
2022-10-27 17:34:17 -0300

    Initial commit

---
+++ aulaedilene.js
@@ -0,0 +1,17 @@
+function numero1 (){ 
+  
+var primeiro = "";
+
+numero1 = document.getElementById('nun1').value;
+document.getElementById('num1').value = primeiro;
+
+
+
+}
+
+function soma (){
+
+    var somaa=(10 + 10)
+}
+
+console.log(soma)
